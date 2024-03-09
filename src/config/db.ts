655b06edc5b349cf6dd79ff579d8e058bbb4dc99
Karla <karla.smuk@gmail.com>
2024-03-09 19:27:43 +0100

    updated docker setup

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,3 @@
-import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 
@@ -8,8 +7,8 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express, {Express, Request, Response} from "express";
 import Application from "./app";
 import dotenv from 'dotenv';
