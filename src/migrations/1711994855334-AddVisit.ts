84538f46672cad9057bf40a94ae8fb81f57c766c
Karla <karla.smuk@gmail.com>
2024-05-10 11:05:39 +0200

    updated visit table

--- src/migrations/1711994855334-AddVisit.ts
+++ src/migrations/1711994855334-AddVisit.ts
@@ -4,7 +4,7 @@ export class AddVisit1711994855334 implements MigrationInterface {
     name = 'AddVisit1711994855334'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "visit" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "time" TIMESTAMP NOT NULL, "weight" double precision NOT NULL, "temperature" double precision NOT NULL, "diagnosis" character varying NOT NULL, "treatment" character varying NOT NULL, "notes" character varying NOT NULL, "petId" uuid, "veterinarianId" uuid, CONSTRAINT "PK_c9919ef5a07627657c535d8eb88" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "visit" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "time" TIMESTAMP NOT NULL, "weight" double precision NOT NULL, "temperature" double precision NOT NULL, "diagnosis" character varying NOT NULL, "notes" character varying NOT NULL, "petId" uuid, "veterinarianId" uuid, CONSTRAINT "PK_c9919ef5a07627657c535d8eb88" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "otp_data" ALTER COLUMN "createdAt" SET DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "otp_data" ALTER COLUMN "attemptsCount" SET DEFAULT '0'`);
         await queryRunner.query(`ALTER TABLE "otp_data" ALTER COLUMN "lastAttemptTime" DROP NOT NULL`);
