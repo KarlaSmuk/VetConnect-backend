f51421af431f2b450754a6f65aea2941d42ed08b
Karla <karla.smuk@gmail.com>
2024-05-17 15:31:04 +0200

    renamed endpoint for sending otp

--- src/routes/otp.router.ts
+++ src/routes/otp.router.ts
@@ -3,7 +3,7 @@ import {  createOtp } from "../controllers/otp.controller";
 
 const router = Router();
 
-router.post("/:userId", createOtp);
+router.post("/sendOTP/:userId", createOtp);
 
 
 export default router;
